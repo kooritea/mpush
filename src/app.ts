4d693f5782fdd603205da0d4b02f046777574913
kooritea <ochatokori@gmail.com>
2019-08-21 14:52:42 +0800

    回复等待改为与超时等待相同

--- src/app.ts
+++ src/app.ts
@@ -12,11 +12,12 @@ const clientManager = new ClientManager(config.WEBHOOK_CLIENTS, async (message) 
         clientManager.sendMessage(message)
         setTimeout(() => {
             resolve(message.getStatus())
-        }, 5000)
+        }, config.PUSH_TIMEOUT)
 
     })
 }, async (mid: number) => {
     // 确认推送成功的mid
+    // 已确认的消息会在Message对象中记录 所以此处无需处理
     // console.log(`已确认推送成功: ` + mid)
 })
 
@@ -28,7 +29,7 @@ new HttpServer(config.HTTP_PORT, async (message) => {
         clientManager.sendMessage(message)
         setTimeout(() => {
             resolve(message.getStatus())
-        }, 5000)
+        }, config.PUSH_TIMEOUT)
 
     })
 })
