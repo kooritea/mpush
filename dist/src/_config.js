4dc8999f5d8d3b6272d46422c1e759dc90a68b80
kooritea <ochatokori@gmail.com>
2019-09-04 15:32:08 +0800

    删除dist

--- dist/src/_config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const config_json_1 = __importDefault(require("../config.json"));
-exports.default = config_json_1.default;
-//# sourceMappingURL=_config.js.map
