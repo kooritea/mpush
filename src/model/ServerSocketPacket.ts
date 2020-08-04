3fa6d3133f3944253e37ffa283d9bde5cecc5c1d
kooritea <ochatokori@gmail.com>
2020-08-04 21:26:10 +0800

    ws认证响应包.webpushServerKey改为.webpushPublicKey

--- src/model/ServerSocketPacket.ts
+++ src/model/ServerSocketPacket.ts
@@ -15,13 +15,13 @@ export class AuthServerSocketPacket extends ServerSocketPacket {
     code: 200 | 401 | 403,
     auth?: string,
     msg: string,
-    webpushServerKey?: string
+    webpushPublicKey?: string
   }
   constructor(data: {
     code: 200 | 401 | 403,
     auth?: string,
     msg: string,
-    webpushServerKey?: string
+    webpushPublicKey?: string
   }) {
     super('AUTH', data)
     this.data = data
